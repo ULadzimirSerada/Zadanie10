1da158cc5541dd5d80758688c406c7c51609971e
UladzimirSerada <medavikiby@gmail.com>
2021-02-05 18:18:55 +0300

    (z10)

---
+++ Zadanie10.java
@@ -0,0 +1,17 @@
+public class Zadanie10{
+	 public static void main(String[] args) {
+/*Задание 10
+Посчитать факториал числа в границах от 10 до 15 (не используя рекурсию).*/
+        public class Main {
+    public static void main(String[] args) {
+        int sum = 1;
+				//int x;
+        for (int x = 10; x <= 15; x++) {
+        sum = sum * x;
+
+            // return sum;
+        }
+
+        System.out.println(sum);
+    }
+}
